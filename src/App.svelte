7a60400ec22eadb28cd90d30445524f185403059
Shawn Lee <changoloco.evg@gmail.com>
2022-04-16 21:24:25 -0500

    :sparkles: [feat]: add profile card component

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,48 @@
-Victor Crest
-26
-London
+<script>
+  import profileImage from './assets/image-victor.jpg';
+</script>
 
-80K
-Followers
+<main>
+  <article
+    class="min-w-xs pt-22.5 p-6 | flex flex-col items-center gap-12 | bg-white rounded-xl shadow-md">
+    <div class="flex flex-col items-center gap-4">
+      <img
+        class="h-24 w-24 min-w-24 rounded-full ring-3 ring-white"
+        width="96"
+        height="96"
+        src={profileImage}
+        alt="Victor Profile" />
+      <header class="flex flex-col items-center gap-1">
+        <div class="flex items-center gap-2">
+          <h1 class="text-xl font-bold">Victor Crest</h1>
+          <span class="text-lg text-cadet-600"> 26 </span>
+        </div>
+        <span class="text-gray"> London </span>
+      </header>
+    </div>
+    <section>
+      <h2 class="sr-only">Stats</h2>
+      <ul class="flex gap-8">
+        <li class="flex flex-col items-center">
+          <span class="text-xl font-bold"> 80K </span>
+          <span class="text-xs text-gray tracking-widest"> Followers </span>
+        </li>
+        <li class="flex flex-col items-center">
+          <span class="text-xl font-bold"> 803K </span>
+          <span class="text-xs text-gray tracking-widest"> Likes </span>
+        </li>
+        <li class="flex flex-col items-center">
+          <span class="text-xl font-bold"> 1.4K </span>
+          <span class="text-xs text-gray tracking-widest"> Photos </span>
+        </li>
+      </ul>
+    </section>
+  </article>
+</main>
 
-803K
-Likes
-
-1.4K
-Photos
+<style>
+  article {
+    background-image: url(./assets/bg-pattern-card.svg);
+    background-repeat: no-repeat;
+  }
+</style>
---
+++ windi.config.js
@@ -0,0 +1,16 @@
+export default {
+  theme: {
+    colors: {
+      cadet: {
+        600: 'hsl(227, 10%, 46%)',
+        800: 'hsl(229, 23%, 23%)',
+      },
+      cyan: 'hsl(185, 75%, 39%)',
+      gray: 'hsl(0, 0%, 59%)',
+      white: '#FFF',
+    },
+    fontFamily: {
+      kumbh: ['Kumbh Sans', 'sans-serif'],
+    },
+  },
+};
